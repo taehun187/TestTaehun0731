e1c99555d016786a8be575be36222093317f0c18
Sukju <tjrwn123456@gmail.com>
2024-07-31 15:07:11 +0900

    edit taehun class

--- src/testrepo/Main.java
+++ src/testrepo/Main.java
@@ -3,10 +3,15 @@ package testrepo;
 public class Main {
 
 	public static void main(String[] args) {
+		Taehun test = new Taehun();
+		
+		test.setAge(100);
+		test.setGender("Male");
+		
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Sukju");
-
+		System.out.println(test.toString());
 	}
 
 }
--- src/testrepo/Taehun.java
+++ src/testrepo/Taehun.java
@@ -27,4 +27,11 @@ public class Taehun {
     public void setAge(int age) {
         this.age = age;
     }
+
+	@Override
+	public String toString() {
+		return "Taehun [gender=" + gender + ", age=" + age + "]";
+	}
+    
+    
 }
