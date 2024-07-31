e9d062de515ef67ad2a0993ba85f71d921da655b
xogns93 <lth1518@gmail.com>
2024-07-31 14:57:08 +0900

    변수 정의와 생성자 게터 세터 메서드

--- src/testrepo/Taehun.java
+++ src/testrepo/Taehun.java
@@ -1,3 +1,28 @@
 public class Taehun {
+    String gender;
+    int age;
 
+    public Taehun() {
+    }
+
+    public Taehun(int age, String gender) {
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
